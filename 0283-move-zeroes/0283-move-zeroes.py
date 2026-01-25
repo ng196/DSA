9de296215a97f2245e3836557eadcd89527bcc37
NITIN GOYAL <166632421+ng196@users.noreply.github.com>
2026-01-25 14:20:16 +0530

    Time: 15 ms (18.57%), Space: 13.4 MB (85.79%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -6,19 +6,15 @@ class Solution(object):
         
         while i < n:
             if nums[i] == 0:
-                # Count consecutive zeros
-                # Added i < n check to prevent IndexError
+
                 while i < n and nums[i] == 0:
                     k += 1 
                     i += 1
             
-            # If we've found zeros (k > 0) and current i is a non-zero
             if i < n and k > 0:
-                # Shift the non-zero element back by k positions
                 nums[i - k] = nums[i]
                 
-                # Zero out the current position
-                # This replaces your nested j-loop logic which was decrementing k
+
                 nums[i] = 0
             
             i += 1
